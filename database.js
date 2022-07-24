3b3cb068921628877d4ec377f4078bc13957c011
bobby-cheema <82983031+bobby-cheema@users.noreply.github.com>
2022-07-25 09:09:24 +1000

    added api authID

--- database.js
+++ database.js
@@ -9,12 +9,24 @@ const pool = mysql
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    // host: "localhost",
+    // user: "root",
+    // password: "laddo123",
+    // database: "my_db",
   })
   .promise();
-
 export async function getNotes() {
   const [rows] = await pool.query("SELECT * FROM user");
   return rows;
+}
+// return authID
+
+export async function getAuthID(fbID) {
+  const AuthID = await pool.query(` SELECT * FROM user where fb_id=?  `, [
+    fbID,
+  ]);
+  console.log("fromdb ", AuthID[0]);
+  return AuthID[0];
 }
 
 // export async function getNote(id) {
@@ -36,3 +48,4 @@ export async function getNotes() {
 // }
 
 // getNotes();
+//mysql://b4ec1a889e974c:2ee55f0b@us-cdbr-east-06.cleardb.net/heroku_a2b76d2c94b8a3f?reconnect=true
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import dotenv from "dotenv";
 import express from "express";
 
-import { getNotes } from "./database.js";
+import { getNotes, getAuthID } from "./database.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-let port = process.env.PORT || 3000;
+let port = 3000;
 
 app.get("/notes", async (req, res) => {
   const notes = await getNotes();
   res.send(notes);
 });
+// Return AuthID
 
+app.get("/AuthID/:id", async (req, res) => {
+  const authID = await getAuthID(req.params.id);
+  console.log("to express", authID);
+  res.send(authID);
+});
 // app.get("/notes/:id", async (req, res) => {
 //   const id = req.params.id
 //   const note = await getNote(id)
@@ -27,10 +33,10 @@ app.get("/notes", async (req, res) => {
 //   res.status(201).send(note)
 // })
 
-// app.use((err, req, res, next) => {
-//   console.error(err.stack)
-//   res.status(500).send('Something broke 💩')
-// })
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke 💩");
+});
 
 app.listen(port, () => {
   console.log("Server is running on port ", port);
